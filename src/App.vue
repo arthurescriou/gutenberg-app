17fbfce6ae741527cedc3ce9adb43e779e27c6f9
arthur <arthur.escriou@gmail.com>
2020-09-22 12:34:12 +0200

    style home page

--- src/App.vue
+++ src/App.vue
@@ -3,3 +3,15 @@
     <router-view />
   </div>
 </template>
+
+<style media="screen">
+body {
+  margin: 0;
+  height: 100vh;
+  width: 100%;
+  background: #efefef;
+}
+#app {
+  height: 100%;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,79 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img src="@/assets/book.png" alt="" />
+    <div class="title">
+      Gutenberg project
+    </div>
+    <div class="links">
+      <div class="link">
+        <router-link to="/lang">Trouver des livres</router-link>
+      </div>
+      <div class="link">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.gutenberg.org/"
+          >Le projet Gutenberg</a
+        >
+      </div>
+      <div class="link">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www-apr.lip6.fr/~buixuan/daar2019"
+          >Le cours de DAAR</a
+        >
+      </div>
+      <div class="link">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/arthurescriou/gutenberg-app"
+          >Le code de cette appli</a
+        >
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style media="screen" scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.title {
+  font-weight: 700;
+  line-height: 1.3em;
+  font-size: 2.3em;
+  padding-bottom: 4rem;
+  text-align: center;
+}
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+.links {
+  display: Grid;
+  grid-gap: 1rem;
+  grid-template-columns: 1fr 1fr;
+}
+
+.link {
+  text-align: center;
+  background: #fefefe;
+  padding: 3rem;
+  border-radius: 12px;
+}
+.link a {
+  padding: 3rem;
+  text-decoration: none;
+  color: black;
+}
+
+@media only screen and (max-width: 800px) {
+  .links {
+    grid-template-columns: 1fr;
+  }
+  img {
+    width: 50%;
   }
 }
-</script>
+</style>
