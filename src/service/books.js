1cfce462feb270cc004a1a633df60853cfe27eb4
arthur <arthur.escriou@gmail.com>
2020-09-22 11:40:43 +0200

    add database

---
+++ src/service/books.js
@@ -0,0 +1,8 @@
+import data from './database.json'
+
+const getBooks = async () =>
+  Object.values(data).reduce((acc, val) => acc.concat(val), [])
+
+const getBooksPerLanguage = async language => data[language]
+
+const getLanguages = async () => Object.keys(data)
